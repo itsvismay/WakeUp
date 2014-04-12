d89d9ce31555e506beebfa6b4bf3d7a7f5d39793
Vismay <itsvismay@gmail.com>
2014-04-12 18:09:05 -0400

    eyes/ not eyes done

--- peyes.py
+++ peyes.py
@@ -5,7 +5,7 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 eyes_open =0
-seconds =0
+time =0
 cap = cv2.VideoCapture(0)
 while(True):
     ret, img =cap.read()
@@ -16,6 +16,15 @@ while(True):
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
+        if(len(eyes)>1):
+            time =0
+            #EYES DETECTED
+            #DO SOMETHING Here
+            print("eyes")
+        else:
+            #EYES NOT DETECTED
+            print("Not")
+            
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
     cv2.imshow('img',img)
